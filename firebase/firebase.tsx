a78c1458419ee9530450580b059a8781bacef99e
sonnysam <samuelagbenyo067@gmail.com>
2023-08-29 08:09:32 +0000

    files created

---
+++ firebase/firebase.tsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+//   apiKey: "AIzaSyDznbLMFmFxLvPS0XB1ytoj7q0H54aMsMA",
+//   authDomain: "ag-pay.firebaseapp.com",
+//   projectId: "ag-pay",
+//   storageBucket: "ag-pay.appspot.com",
+//   messagingSenderId: "380201047463",
+//   appId: "1:380201047463:web:3ed4571ea016d86afc1282",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth();
+
+export { auth, db };
+
+// // firebase": "^10.1.0",
--- navigation/AuthStack.tsx
+++ navigation/AuthStack.tsx
@@ -1,17 +1,7 @@
-import React, { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { User, onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase/firebase";
-
-import Hello from "../screens/Hello";
-import { UserDashboard } from "./InitialScreenOnStart";
-import Confirm from "../screens/Confirm";
-import Admin from "../screens/Admin";
-import AllUsers from "../screens/AllUsers";
-import UserComplaint from "../screens/UserComplaint";
-import AdminLogin from "../screens/AdminLogin";
+import Dashboard from "../screens/Dashboard";
+import Profile from "../screens/Profile";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,50 +9,15 @@ export default function AuthStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="Hello"
-        component={Hello}
+        name="Dashboard"
+        component={Dashboard}
         options={{
           headerShown: false,
         }}
       />
       <Stack.Screen
-        name="UserDashboard"
-        component={UserDashboard}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Confirm"
-        component={Confirm}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Admin"
-        component={Admin}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="AdminLogin"
-        component={AdminLogin}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="AllUsers"
-        component={AllUsers}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="UserComplaints"
-        component={UserComplaint}
+        name="Profile"
+        component={Profile}
         options={{
           headerShown: false,
         }}
