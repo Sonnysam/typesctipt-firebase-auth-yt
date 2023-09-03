b7690883584dc164019113d2fdbe69a32a0b624b
sonnysam <samuelagbenyo067@gmail.com>
2023-09-03 16:38:48 +0000

    yarn removed and npm used-project refactored

--- README.md
+++
@@ -1,2 +0,0 @@
-# Typesctipt Firebase Auth 
-A react native expo app with firebase authentication integration using typescript.
--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -2,21 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDznbLMFmFxLvPS0XB1ytoj7q0H54aMsMA",
-  authDomain: "ag-pay.firebaseapp.com",
-  projectId: "ag-pay",
-  storageBucket: "ag-pay.appspot.com",
-  messagingSenderId: "380201047463",
-  appId: "1:380201047463:web:3ed4571ea016d86afc1282",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
 const db = getFirestore(app);
-const auth = getAuth();
 
 export { auth, db };
-
-// // firebase": "^10.1.0",
