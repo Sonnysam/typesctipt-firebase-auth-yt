eb9655485f122d93086cc2bf5a1b8c5b06a037b2
sonnysam <samuelagbenyo067@gmail.com>
2023-09-06 10:08:09 +0000

    add and fetch data functionality added

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -6,15 +6,17 @@ import {
   TextInput,
   Platform,
   Dimensions,
-  Alert
-
+  Alert,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Colors from "../constants/Colors";
 import { Feather } from "@expo/vector-icons";
 import { auth, db } from "../firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 export default function Dashboard({ navigation }: { navigation: any }) {
+  const [userInfo, setUserInfo] = useState<any | undefined>(null);
+
   const handleSignout = async () => {
     await auth.signOut();
   };
@@ -27,12 +29,27 @@ export default function Dashboard({ navigation }: { navigation: any }) {
       { text: "Logout", onPress: handleSignout },
     ]);
   };
+
+  const getData = async () => {
+    const docRef = doc(db, "users", "info");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setUserInfo(docSnap.data());
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <View style={styles.container}>
-        <Text style={{fontSize: 25}}>Welcome Fam!</Text>
+      <Text style={{ fontSize: 25 }}>Welcome Fam!</Text>
       <View>
         <TouchableOpacity style={styles.button} onPress={Modal}>
-          <Text style={{color: Colors.white, fontSize: 20}}>Sign out</Text>
+          <Text style={{ color: Colors.white, fontSize: 20 }}>Sign out</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -45,7 +62,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  button:{
+  button: {
     backgroundColor: Colors.primary,
     padding: 10,
     borderRadius: 8,
